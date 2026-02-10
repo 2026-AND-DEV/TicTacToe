0907a4e1091705548c112e767428061d642329ab
2026-AND-DEV <43pixelz@gmail.com>
2026-02-10 18:27:32 +0530

    Updated unit test in GameViewModelTest to validate error handling for invalid move index

--- app/src/test/java/com/example/tictactoe/presentation/viewmodel/GameViewModelTest.kt
+++ app/src/test/java/com/example/tictactoe/presentation/viewmodel/GameViewModelTest.kt
@@ -6,6 +6,7 @@ import com.example.tictactoe.domain.model.GameState
 import com.example.tictactoe.domain.model.MovementResult
 import com.example.tictactoe.domain.usecase.GamePlayUseCase
 import com.example.tictactoe.utils.BOARD_SIZE
+import com.example.tictactoe.utils.INVALID_INDEX
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -38,13 +39,15 @@ class GameViewModelTest {
     }
 
     @Test
-    fun `Check is make move intent is calling gameplay usecase`() {
+    fun `Check is make move intent is calling gameplay usecase and return error`() {
         // Arrange
-        every { gamePlayUseCase.makeMove(any(), any(), any()) } returns MovementResult.Error("")
+        every { gamePlayUseCase.makeMove(-1, 0, any()) } returns MovementResult.Error(
+            INVALID_INDEX
+        )
         // Act
-        viewModel.onIntent(GameIntents.MakeMove(0, 0))
+        viewModel.onIntent(GameIntents.MakeMove(-1, 0))
         // Assert
-        verify { gamePlayUseCase.makeMove(any(),any(), any()) }
+        verify { gamePlayUseCase.makeMove(-1,0, any()) }
     }
 
 }
