683cdad8491b1a135d231467b2910024109b25d9
2026-AND-DEV <43pixelz@gmail.com>
2026-02-10 14:48:22 +0530

    Refactored row and column index validation in GamePlayUseCase to use BOARD_SIZE constant

--- app/src/main/java/com/example/tictactoe/domain/usecase/GamePlayUseCase.kt
+++ app/src/main/java/com/example/tictactoe/domain/usecase/GamePlayUseCase.kt
@@ -1,14 +1,15 @@
 package com.example.tictactoe.domain.usecase
 
+import com.example.tictactoe.utils.BOARD_SIZE
 import com.example.tictactoe.utils.INVALID_COLUMN_INDEX
 import com.example.tictactoe.utils.INVALID_ROW_INDEX
 
 class GamePlayUseCase {
     fun makeMove(row: Int, col: Int) : String {
-        if (row !in 0..2) {
+        if (row !in 0..<BOARD_SIZE) {
             return INVALID_ROW_INDEX
         }
-        if (col !in 0..2) {
+        if (col !in 0..<BOARD_SIZE) {
             return INVALID_COLUMN_INDEX
         }
         return ""
--- app/src/main/java/com/example/tictactoe/utils/Constants.kt
+++ app/src/main/java/com/example/tictactoe/utils/Constants.kt
@@ -2,3 +2,4 @@ package com.example.tictactoe.utils
 
 const val INVALID_ROW_INDEX = "Invalid row index"
 const val INVALID_COLUMN_INDEX = "Invalid column index"
+const val BOARD_SIZE = 3
