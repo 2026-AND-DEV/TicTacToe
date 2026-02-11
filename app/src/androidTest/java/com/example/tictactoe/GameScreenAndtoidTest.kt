1a20b7500e8a2a78620847a1bf1b9c8c2ce9a357
2026-AND-DEV <43pixelz@gmail.com>
2026-02-11 17:32:22 +0530

    Refactored GameScreenAndroidTest

--- app/src/androidTest/java/com/example/tictactoe/GameScreenAndtoidTest.kt
+++ app/src/androidTest/java/com/example/tictactoe/GameScreenAndtoidTest.kt
@@ -9,11 +9,14 @@ import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.rememberNavController
+import com.example.tictactoe.domain.model.Player
 import com.example.tictactoe.presentation.ui.navigation.TicTacToeNavGraph
 import com.example.tictactoe.presentation.ui.theme.TicTacToeTheme
 import com.example.tictactoe.utils.BOARD_SIZE
 import com.example.tictactoe.utils.EMPTY_STRING
 import com.example.tictactoe.utils.GAME_OVER_DRAW
+import com.example.tictactoe.utils.PLAYER_WON
+import com.example.tictactoe.utils.TURN_TO_PLAY
 import com.example.tictactoe.utils.TestTags
 import org.junit.Before
 import org.junit.Rule
@@ -47,49 +50,53 @@ class GameScreenAndtoidTest {
 
     @Test
     fun `check is player x turn displayed after app launch`() {
+        val turnMessage = String.format(TURN_TO_PLAY, Player.X.name)
         composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertIsDisplayed()
-        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals("X's turn to play")
+        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals(turnMessage)
     }
 
     @Test
     fun `check is cell updates on user clicks on empty cell`() {
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[0].performClick()
-        composeTestRule.onAllNodesWithTag(TestTags.CELL)[0].assertTextEquals("X")
+        composeTestRule.onAllNodesWithTag(TestTags.CELL)[0].assertTextEquals(Player.X.name)
     }
 
     @Test
     fun `check is info text updates on user clicks on empty cell`() {
+        val turnMessage = String.format(TURN_TO_PLAY, Player.O.name)
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[0].performClick()
-        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals("O's turn to play")
+        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals(turnMessage)
     }
 
     @Test
     fun `check is players turn switched and updated on user clicks on empty cell`() {
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[0].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[1].performClick()
-        composeTestRule.onAllNodesWithTag(TestTags.CELL)[1].assertTextEquals("O")
+        composeTestRule.onAllNodesWithTag(TestTags.CELL)[1].assertTextEquals(Player.O.name)
     }
 
     @Test
     fun `check is player win displayed on horizontal win`() {
+        val winMessage = String.format(PLAYER_WON, Player.X.name)
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[0].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[8].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[1].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[7].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[2].performClick()
-        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals("Player X won")
+        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals(winMessage)
         composeTestRule.onNodeWithTag(TestTags.SNACKBAR).assertIsDisplayed()
     }
 
     @Test
     fun `check is player win displayed on vertical win`() {
+        val winMessage = String.format(PLAYER_WON, Player.O.name)
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[0].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[2].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[1].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[5].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[3].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[8].performClick()
-        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals("Player O won")
+        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals(winMessage)
         composeTestRule.onNodeWithTag(TestTags.SNACKBAR).assertIsDisplayed()
     }
 
@@ -110,15 +117,17 @@ class GameScreenAndtoidTest {
 
     @Test
     fun `check is game is reset and cell are cleared on reset click`() {
+        val winMessage = String.format(PLAYER_WON, Player.O.name)
+        val turnMessage = String.format(TURN_TO_PLAY, Player.X.name)
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[0].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[2].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[1].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[5].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[3].performClick()
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[8].performClick()
-        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals("Player O won")
+        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals(winMessage)
         composeTestRule.onNodeWithTag(TestTags.RESET_BUTTON).performClick()
-        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals("X's turn to play")
+        composeTestRule.onNodeWithTag(TestTags.INFO_TEXT).assertTextEquals(turnMessage)
         composeTestRule.onAllNodesWithTag(TestTags.CELL)[0].assertTextEquals(EMPTY_STRING)
     }
 
